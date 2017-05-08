721f1f23f67fa47dff97ddf6514c75b1920fa158
Karolina Dudzik <kaardud@gmail.com>
2017-05-08 21:47:35 +0200

    game elements js

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ var gameState = 'notStarted',  //started // ended
     computer = {
         score: 0
     };
+
+var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+  switch(gameState) {
+    case 'started':
+        newGameElem.style.display = 'none';
+        pickElem.style.display = 'block';
+        resultsElem.style.display = 'block';
+      break;
+    case 'ended':
+        newGameBtn.innerText = 'Jeszcze raz';
+    case 'notStarted':
+    default:
+        newGameElem.style.display = 'block';
+        pickElem.style.display = 'none';
+        resultsElem.style.display = 'none';
+  }
+};
+
+setGameElements();
