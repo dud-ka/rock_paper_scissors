09296c634c9e8449f0d8e460ae12e417e90c700e
Karolina Dudzik <kaardud@gmail.com>
2017-05-08 21:10:21 +0200

    Initial values

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ pickRock.addEventListener('click', function() { playerPick('rock') });
 pickPaper.addEventListener('click', function() { playerPick('paper') });
 pickScissors.addEventListener('click', function() { playerPick('scissors') });
 //end pick player
+
+var gameState = 'notStarted',  //started // ended
+    player = {
+        name: '',
+        score: 0
+    },
+    computer = {
+        score: 0
+    };
